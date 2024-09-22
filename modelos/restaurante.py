13993e531dd91ac6eb728a78388bbda888865741
Edy Souza <edysouza363@gmail.com>
2024-09-22 07:54:33 -0300

    Construtor

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -6,7 +6,11 @@ class Restaurante:
 
 # instanciando uma classe com seu objeto    
 restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = 'Gourmet'
+restaurante_praca.ativo = False
 
+
+restaurante_pizza = Restaurante()
 restarantes = [restaurante_pizza , restaurante_pizza]
 print(restarantes)
