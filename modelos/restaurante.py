6dfb5c8c0aeab1cab7535d87c11a53ff1adce504
Edy Souza <edysouza363@gmail.com>
2024-09-22 07:36:50 -0300

    Criando Classes

---
+++ modelos/restaurante.py
@@ -0,0 +1,12 @@
+# Criando as classe do restaurante
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+# instanciando uma classe com seu objeto    
+restaurante_praca = Restaurante()
+restaurante_pizza = Restaurante()
+
+restarantes = [restaurante_pizza , restaurante_pizza]
+print(restarantes)
