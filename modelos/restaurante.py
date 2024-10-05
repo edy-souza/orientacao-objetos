ec8e2cae10aee82c44eefc135cdb28f371fe9248
Edy Souza <edysouza363@gmail.com>
2024-10-05 17:14:39 -0300

    Instanciando Objetos

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,17 +1,22 @@
 # Criando as classe do restaurante
-class Restaurante:
+class Restaurante: # por padrão o nome da classe de iniciar com letra Maiúscula
+    restaurantes = []
+    
     def __init__ (self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self) #adiciona restaurante na lista restauranteS
         
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')            
        
 # instanciando uma classe com seu objeto    
 restaurante_praca = Restaurante('Praça' , 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express' , 'Italiano')
 
-restarantes = [restaurante_pizza , restaurante_pizza]
-print(restaurante_pizza)
-print(restaurante_praca)
+Restaurante.listar_restaurantes()
