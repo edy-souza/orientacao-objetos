476f0740536cd9fdb9c73356fe73f14738bee251
Edy Souza <edysouza363@gmail.com>
2024-10-05 19:03:52 -0300

    Property

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -5,15 +5,20 @@ class Restaurante: # por padrão o nome da classe de iniciar com letra Maiúscul
     def __init__ (self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self) #adiciona restaurante na lista restauranteS
         
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
     
     def listar_restaurantes():
+        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')            
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')      
+    
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'      
        
 # instanciando uma classe com seu objeto    
 restaurante_praca = Restaurante('Praça' , 'Gourmet')
