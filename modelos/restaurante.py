4db3388e06f432c8c7009e375a347c5b7e1962e8
Edy Souza <edysouza363@gmail.com>
2024-10-06 11:27:11 -0300

    Métodos de classes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -3,25 +3,25 @@ class Restaurante: # por padrão o nome da classe de iniciar com letra Maiúscul
     restaurantes = []
     
     def __init__ (self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()  # Transforma a primeira letra em Maiúscula.
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self) #adiciona restaurante na lista restauranteS
         
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
     def listar_restaurantes():
         print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(25)} | {"Status"}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')      
-    
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')      
+
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐'      
        
 # instanciando uma classe com seu objeto    
-restaurante_praca = Restaurante('Praça' , 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express' , 'Italiano')
+restaurante_praca = Restaurante('praça' , 'Gourmet')
+restaurante_pizza = Restaurante('pizza Express' , 'Italiano')
 
 Restaurante.listar_restaurantes()
