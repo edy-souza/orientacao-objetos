db9372e264811c5c369f1fe62292c75aa62fa523
Edy Souza <edysouza363@gmail.com>
2024-10-15 19:51:54 -0300

    Update

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from modelos.restaurante import Restaurante
 restaurante_praca = Restaurante('Praça' , 'Goumert')
 restaurante_praca.recber_avaliacao('Gustavo', 10)
 restaurante_praca.recber_avaliacao('Bernardo', 8)
+restaurante_praca.recber_avaliacao('Luiza', 5)
+restaurante_praca.recber_avaliacao('Lukas', 8)
 def main():
     Restaurante.listar_restaurantes()
 
