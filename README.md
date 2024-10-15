41bc33bc6f2d63e4ccfe01642ae500a9de64909d
Edy Souza <edysouza363@gmail.com>
2024-10-15 20:44:42 -0300

    Update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ## APP Orientado a objetos
+
+* Classes
+* Construtor e instanciando objetos
+* Property e métodos de classe
+* Importando classe e composição
+* Consolidando os conhecimentos
+
+# Tecnologias
+* Python
+* Git
+* GitHub
+* VsCode
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ restaurante_praca = Restaurante('Praça' , 'Goumert')
 restaurante_praca.recber_avaliacao('Gustavo', 10)
 restaurante_praca.recber_avaliacao('Bernardo', 8)
 restaurante_praca.recber_avaliacao('Luiza', 5)
-restaurante_praca.recber_avaliacao('Lukas', 8)
+restaurante_praca.recber_avaliacao('Lukas', 3)
 def main():
     Restaurante.listar_restaurantes()
 
